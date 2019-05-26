8906f009c1f72c836fcb9fcbbfe909b6ade54ea6
Howard Choi <hchoi84@gmail.com>
2019-05-26 14:46:21 -0700

    updated variables from multiple arrays to one object. implemented element colors. created borders around the element. correct borders from being applied to empty spaces.

--- script.js
+++ script.js
@@ -1,39 +1,68 @@
 var squareSize = 40;
-var symbol = {
-    1: "H",    2: "He",    3: "Li",    4: "Be",    5: "B",    6: "C",    7: "N",    8: "O",    9: "F",    10: "Ne",
-    11: "Na",    12: "Mg",    13: "Al",    14: "Si",    15: "P",    16: "S",    17: "Cl",    18: "Ar",    19: "K",
-    20: "Ca",    21: "Sc",    22: "Ti",    23: "V",    24: "Cr",    25: "Mn",    26: "Fe",    27: "Co",    28: "Ni",
-    29: "Cu",    30: "Zn",    31: "Ga",    32: "Ge",    33: "As",    34: "Se",    35: "Br",    36: "Kr",    37: "Rb",
-    38: "Sr",    39: "Y",    40: "Zr",    41: "Nb",    42: "Mo",    43: "Tc",    44: "Ru",    45: "Rh",    46: "Pd",
-    47: "Ag",    48: "Cd",    49: "In",    50: "Sn",    51: "Sb",    52: "Te",    53: "I",    54: "Xe",    55: "Cs",
-    56: "Ba",    57: "La",    58: "Ce",    59: "Pr",    60: "Nd",    61: "Pm",    62: "Sm",    63: "Eu",    64: "Gd",
-    65: "Tb",    66: "Dy",    67: "Ho",    68: "Er",    69: "Tm",    70: "Yb",    71: "Lu",    72: "Hf",    73: "Ta",
-    74: "W",    75: "Re",    76: "Os",    77: "Ir",    78: "Pt",    79: "Au",    80: "Hg",    81: "Tl",    82: "Pb",
-    83: "Bi",    84: "Po",    85: "At",    86: "Rn",    87: "Fr",    88: "Ra",    89: "Ac",    90: "Th",    91: "Pa",
-    92: "U",    93: "Np",    94: "Pu",    95: "Am",    96: "Cm",    97: "Bk",    98: "Cf",    99: "Es",    100: "Fm",
-    101: "Md",    102: "No",    103: "Lr",    104: "Rf",    105: "Db",    106: "Sg",    107: "Bh",    108: "Hs",    109: "Mt",
-    110: "Ds",    111: "Rg",    112: "Cn",    113: "Nh",    114: "Fl",    115: "Mc",    116: "Lv",    117: "Ts",    118: "Og"
-}
-
-var mass = {
-    1: 1.008,    2: 4.003,    3: 6.968,    4: 9.012,    5: 10.81,    6: 12.01,    7: 14.01,    8: 16.00,    9: 19.00,    10: 20.18,
-    11: 22.99,    12: 24.31,    13: 26.98,    14: 28.09,    15: 30.97,    16: 32.07,    17: 35.45,    18: 39.95,    19: 39.10,
-    20: 40.08,    21: 44.96,    22: 47.87,    23: 50.94,    24: 52.00,    25: 54.94,    26: 55.85,    27: 58.93,    28: 58.69,
-    29: 63.55,    30: 65.38,    31: 69.72,    32: 72.63,    33: 74.92,    34: 78.97,    35: 79.90,    36: 83.80,    37: 85.47,
-    38: 87.62,    39: 88.91,    40: 91.22,    41: 92.91,    42: 95.95,    43: 98,    44: 101.1,    45: 102.9,    46: 106.4,
-    47: 107.9,    48: 112.4,    49: 114.8,    50: 118.7,    51: 121.8,    52: 127.6,    53: 126.9,    54: 131.3,    55: 132.9,
-    56: 137.3,    57: 138.9,    58: 140.1,    59: 140.9,    60: 144.2,    61: 145,    62: 150.4,    63: 152.0,    64: 157.3,
-    65: 158.9,    66: 162.5,    67: 164.9,    68: 167.3,    69: 168.9,    70: 173.1,    71: 175.0,    72: 178.5,    73: 180.9,
-    74: 183.8,    75: 186.2,    76: 190.2,    77: 192.2,    78: 195.1,    79: 197.0,    80: 200.6,    81: 204.4,    82: 207.2,
-    83: 209.0,    84: 209,    85: 210,    86: 222,    87: 223,    88: 226,    89: 227,    90: 232.0,    91: 231.0,    92: 238.0,
-    93: 237,    94: 244,    95: 243,    96: 247,    97: 247,    98: 251,    99: 252,    100: 257,    101: 258,    102: 259,
-    103: 262,    104: 267,    105: 268,    106: 271,    107: 272,    108: 270,    109: 276,    110: 281,    111: 280,    112: 285,
-    113: 284,    114: 289,    115: 288,    116: 293,    117: 292,    118: 294,
-}
-
+var elements =[
+    { symbol: "H", mass: 1.008, color: "blue" },    { symbol: "He", mass: 4.003, color: "yellow" },
+    { symbol: "Li", mass: 6.968, color: "blue" },    { symbol: "Be", mass: 9.012, color: "blue" },
+    { symbol: "B", mass: 10.81, color: "yellow" },    { symbol: "C", mass: 12.01, color: "yellow" },
+    { symbol: "N", mass: 14.01, color: "yellow" },    { symbol: "O", mass: 16.00, color: "yellow" },
+    { symbol: "F", mass: 19.00, color: "yellow" },    { symbol: "Ne", mass: 20.18, color: "yellow" },
+    { symbol: "Na", mass: 22.99, color: "blue" },    { symbol: "Mg", mass: 24.31, color: "blue" },
+    { symbol: "Al", mass: 26.98, color: "yellow" },    { symbol: "Si", mass: 28.09, color: "yellow" },
+    { symbol: "P", mass: 30.97, color: "yellow" },    { symbol: "S", mass: 32.07, color: "yellow" },
+    { symbol: "Cl", mass: 35.45, color: "yellow" },    { symbol: "Ar", mass: 39.95, color: "yellow" },
+    { symbol: "K", mass: 39.10, color: "blue" },    { symbol: "Ca", mass: 40.08, color: "blue" },
+    { symbol: "Sc", mass: 44.96, color: "red" },    { symbol: "Ti", mass: 47.87, color: "red" },
+    { symbol: "V", mass: 50.94, color: "red" },    { symbol: "Cr", mass: 52.00, color: "red" },
+    { symbol: "Mn", mass: 54.94, color: "red" },    { symbol: "Fe", mass: 55.85, color: "red" },
+    { symbol: "Co", mass: 58.93, color: "red" },    { symbol: "Ni", mass: 58.69, color: "red" },
+    { symbol: "Cu", mass: 63.55, color: "red" },    { symbol: "Zn", mass: 65.38, color: "red" },
+    { symbol: "Ga", mass: 69.72, color: "yellow" },    { symbol: "Ge", mass: 72.63, color: "yellow" },
+    { symbol: "As", mass: 74.92, color: "yellow" },    { symbol: "Se", mass: 78.97, color: "yellow" },
+    { symbol: "Br", mass: 79.90, color: "yellow" },    { symbol: "Kr", mass: 83.80, color: "yellow" },
+    { symbol: "Rb", mass: 85.47, color: "blue" },    { symbol: "Sr", mass: 87.62, color: "blue" },
+    { symbol: "Y", mass: 88.91, color: "red" },    { symbol: "Zr", mass: 91.22, color: "red" },
+    { symbol: "Nb", mass: 92.91, color: "red" },    { symbol: "Mo", mass: 95.95, color: "red" },
+    { symbol: "Tc", mass: 98, color: "red" },    { symbol: "Ru", mass: 101.1, color: "red" },
+    { symbol: "Rh", mass: 102.9, color: "red" },    { symbol: "Pd", mass: 106.4, color: "red" },
+    { symbol: "Ag", mass: 107.9, color: "red" },    { symbol: "Cd", mass: 112.4, color: "red" },
+    { symbol: "In", mass: 114.8, color: "yellow" },    { symbol: "Sn", mass: 118.7, color: "yellow" },
+    { symbol: "Sb", mass: 121.8, color: "yellow" },    { symbol: "Te", mass: 127.6, color: "yellow" },
+    { symbol: "I", mass: 126.9, color: "yellow" },    { symbol: "Xe", mass: 131.3, color: "yellow" },
+    { symbol: "Cs", mass: 132.9, color: "blue" },    { symbol: "Ba", mass: 137.3, color: "blue" },
+    { symbol: "La", mass: 138.9, color: "green" },    { symbol: "Ce", mass: 140.1, color: "green" },
+    { symbol: "Pr", mass: 140.9, color: "green" },    { symbol: "Nd", mass: 144.2, color: "green" },
+    { symbol: "Pm", mass: 145, color: "green" },    { symbol: "Sm", mass: 150.4, color: "green" },
+    { symbol: "Eu", mass: 152.0, color: "green" },    { symbol: "Gd", mass: 157.3, color: "green" },
+    { symbol: "Tb", mass: 158.9, color: "green" },    { symbol: "Dy", mass: 162.5, color: "green" },
+    { symbol: "Ho", mass: 164.9, color: "green" },    { symbol: "Er", mass: 167.3, color: "green" },
+    { symbol: "Tm", mass: 168.9, color: "green" },    { symbol: "Yb", mass: 173.1, color: "green" },
+    { symbol: "Lu", mass: 175.0, color: "red" },    { symbol: "Hf", mass: 178.5, color: "red" },
+    { symbol: "Ta", mass: 180.9, color: "red" },    { symbol: "W", mass: 183.8, color: "red" },
+    { symbol: "Re", mass: 186.2, color: "red" },    { symbol: "Os", mass: 190.2, color: "red" },
+    { symbol: "Ir", mass: 192.2, color: "red" },    { symbol: "Pt", mass: 195.1, color: "red" },
+    { symbol: "Au", mass: 197.0, color: "red" },    { symbol: "Hg", mass: 200.6, color: "red" },
+    { symbol: "Tl", mass: 204.4, color: "yellow" },    { symbol: "Pb", mass: 207.2, color: "yellow" },
+    { symbol: "Bi", mass: 209.0, color: "yellow" },    { symbol: "Po", mass: 209, color: "yellow" },
+    { symbol: "At", mass: 210, color: "yellow" },    { symbol: "Rn", mass: 222, color: "yellow" },
+    { symbol: "Fr", mass: 223, color: "blue" },    { symbol: "Ra", mass: 226, color: "blue" },
+    { symbol: "Ac", mass: 227, color: "green" },    { symbol: "Th", mass: 232.0, color: "green" },
+    { symbol: "Pa", mass: 231.0, color: "green" },    { symbol: "U", mass: 238.0, color: "green" },
+    { symbol: "Np", mass: 237, color: "green" },    { symbol: "Pu", mass: 244, color: "green" },
+    { symbol: "Am", mass: 243, color: "green" },    { symbol: "Cm", mass: 247, color: "green" },
+    { symbol: "Bk", mass: 247, color: "green" },    { symbol: "Cf", mass: 251, color: "green" },
+    { symbol: "Es", mass: 252, color: "green" },    { symbol: "Fm", mass: 257, color: "green" },
+    { symbol: "Md", mass: 258, color: "green" },    { symbol: "No", mass: 259, color: "green" },
+    { symbol: "Lr", mass: 262, color: "red" },    { symbol: "Rf", mass: 267, color: "red" },
+    { symbol: "Db", mass: 268, color: "red" },    { symbol: "Sg", mass: 271, color: "red" },
+    { symbol: "Bh", mass: 272, color: "red" },    { symbol: "Hs", mass: 270, color: "red" },
+    { symbol: "Mt", mass: 276, color: "red" },    { symbol: "Ds", mass: 281, color: "red" },
+    { symbol: "Rg", mass: 280, color: "red" },    { symbol: "Cn", mass: 285, color: "red" },
+    { symbol: "Nh", mass: 284, color: "yellow" },    { symbol: "Fl", mass: 289, color: "yellow" },
+    { symbol: "Mc", mass: 288, color: "yellow" },    { symbol: "Lv", mass: 293, color: "yellow" },
+    { symbol: "Ts", mass: 292, color: "yellow" },    { symbol: "Og", mass: 294, color: "yellow" }
+]
 var table = [
     1,  0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   2,
-    3,  4,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   5,   6,   7,   8,   9,   11,
+    3,  4,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   5,   6,   7,   8,   9,   10,
     11, 12, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   13,  14,  15,  16,  17,  18,
     19, 20, 21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  36,
     37, 38, 39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,
@@ -51,18 +80,19 @@ function drawSquares(){
     for (var y = 1; y <= 9; y++){
         output += "<div class='row" + y + "'>"
         for (var x = 0; x < 18; x++){
-            output += "<div class='squares'>";
             if(table[tableNum] == 0){
+                output += "<div class='blank'></div>";
                 tableNum++
             }
             else{
+                output += "<div class='squares " + elements[table[tableNum] - 1]["color"] + "'>";
                 output += ("<p>" + table[tableNum] + "</p>");
-                output += ("<h3>" + symbol[table[tableNum]] + "</h3>");
-                output += ("<p>" + mass[table[tableNum]] + "</p>");
+                output += ("<h3>" + elements[table[tableNum] - 1]["symbol"] + "</h3>");
+                output += ("<p>" + elements[table[tableNum] - 1]["mass"] + "</p>");
                 tableNum++;
                 atomicNum ++;
+                output += "</div>";
             }
-            output += "</div>";
         }
         output += "</div>"
     }
